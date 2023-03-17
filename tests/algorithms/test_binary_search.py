c1ad62c75b011f56b2156b650d582cbc6e59ae1c
Daniel Gibson <gibsonlp@live.co.uk>
2023-03-17 14:40:40 +0000

    .

--- tests/algorithms/test_binary_search.py
+++ tests/algorithms/test_binary_search.py
@@ -1,8 +1,4 @@
 import unittest
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 from src.algorithms.binary_search import BinarySearch
 
 
